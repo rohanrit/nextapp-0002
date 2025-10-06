9d1a19fcfe9e2e1367f7113ef6dbb2f8e72c60cc
Ritesh Rohan <46083169+rohanrit@users.noreply.github.com>
2025-10-06 13:28:37 +0530

    install best auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav className="flex items-center gap-4">
+          <Link href={'/'}>Home</Link>
+          <Link href={'/dashboard'}>Dashboard</Link>
+          <Link href={'/signin'}>Signin</Link>
+          <Link href={'/signup'}>Signup</Link>
+        </nav>
         {children}
       </body>
     </html>
