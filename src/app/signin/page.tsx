2cb2fc6baa39e9b3a4fc7e510287b4856e78e472
Ritesh Rohan <46083169+rohanrit@users.noreply.github.com>
2025-10-06 15:56:52 +0530

    sign in build

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+"use client"
+import { authClient } from '@/lib/auth-client';
+import React, { useState } from 'react';
 
 const SigninPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const { data, error } = await authClient.signIn.email({
+      email,
+      password,
+      callbackURL: "/dashboard",
+      rememberMe: true,
+    }, {
+      //callbacks
+    });
+  }
+
   return (
-    <div>SigninPage</div>
+    <div className="flex items-center justify-center h-screen">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 bg-gray-200 p-4 mx-auto">
+        <h3>SignIn</h3>
+        <input className="border border-gray-600 p-2" type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <input className="border border-gray-600 p-2" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <button className="bg-black text-white p-2 cursor-pointer" type="submit">Signin</button>
+      </form>
+    </div>
   )
 }
 
